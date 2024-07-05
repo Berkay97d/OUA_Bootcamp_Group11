9a1fad3b38d44e49e1cb204454ee341939e94935
Bora Sevim <sevimbora@gmail.com>
2024-07-05 15:17:54 +0300

    8-way moveable tiles for pieces

--- Assets/_Scripts/Chess Pieces/ChessPiece.cs
+++ Assets/_Scripts/Chess Pieces/ChessPiece.cs
@@ -21,5 +21,49 @@ public class ChessPiece : MonoBehaviour
     public ChessPieceType type;
 
     private Vector3 desiredPosition;
-    private Vector3 desiredScale;
+    private Vector3 desiredScale = Vector3.one;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 10);
+        transform.localScale = Vector3.Lerp(transform.localScale, desiredScale, Time.deltaTime * 10);
+    }
+
+    public List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX)
+    {
+        List<Vector2Int> r = new List<Vector2Int>();
+        var allMoves = new int[,] {
+            {currentX - 1, currentY + 1},
+            {currentX, currentY + 1},
+            {currentX + 1, currentY + 1},
+            {currentX - 1, currentY},
+            {currentX + 1, currentY},
+            {currentX - 1, currentY - 1},
+            {currentX - 1, currentY - 1},
+            {currentX + 1, currentY - 1}
+        };
+        
+        for(int i = 0; i < allMoves.Length; i++)
+        {
+            if(board[allMoves[i,0], allMoves[i,1]] == null)
+            {
+                r.Add(new Vector2Int(currentX, currentY + 1));
+            }
+        }
+
+        return r;
+    }
+    public virtual void SetPosition(Vector3 position, bool force = false)
+    {
+        desiredPosition = position;
+        if(force)
+            transform.position = desiredPosition;
+    }
+
+    public virtual void SetScale(Vector3 scale, bool force = false)
+    {
+        desiredScale = scale;
+        if(force)
+            transform.localScale = desiredScale;
+    }
 }
