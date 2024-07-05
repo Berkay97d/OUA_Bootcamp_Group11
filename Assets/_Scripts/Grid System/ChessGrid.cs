ed7b9b94b7c36b3f2cecda90a3ba22358724af3d
Berkay Dindar <berkay97d@gmail.com>
2024-07-05 05:29:59 +0300

    Chess Grid Singleton

--- Assets/_Scripts/Grid System/ChessGrid.cs
+++ Assets/_Scripts/Grid System/ChessGrid.cs
@@ -9,15 +9,26 @@ namespace _Scripts.Grid_System
         [SerializeField] private Transform _tilePrefabs;
         [SerializeField] private float _tileSize;
 
+        private static ChessGrid ms_Instance;
         private const int GRID_WIDTH = 8;
         private const int GRID_HEIGHT = 8;
         private GridSystem m_gridSystem;
-        
-        
+
+
+        private void Awake()
+        {
+            ms_Instance = this;
+        }
+
         private void Start()
         {
             m_gridSystem = new GridSystem(GRID_WIDTH, GRID_HEIGHT, _tileSize);
             m_gridSystem.CreateTiles(_tilePrefabs);
+        }
+
+        public GridSystem GetGridSystem()
+        {
+            return ms_Instance.m_gridSystem;
         }
         
     }
