be3a7fd59fdd23ec74107d1614870ee8c39b4073
Bora Sevim <sevimbora@gmail.com>
2024-07-17 11:18:59 +0300

    knight attack pattern implemented

--- Assets/_Scripts/Chess Pieces/Knight.cs
+++ Assets/_Scripts/Chess Pieces/Knight.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using _Scripts.Grid_System;
 using UnityEngine;
 
 namespace ChessPieces
 {
-public class Knight : ChessPiece
-{
-    
-}
+    public class Knight : ChessPiece
+    {
+        public override List<GridObject> GetAttackPattern()
+        {
+            List<GridObject> attackTiles = new List<GridObject>();
+
+            var piecePos = this.GetGridPosition();
+            var _gridObject = m_gridSystem.GetGridObject(piecePos);
+
+            // 2-up 1-left, 2-up 1-right
+            attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x - 1, piecePos._z + 2)));
+            attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x + 1, piecePos._z + 2)));
+            
+            // 1-up 2-left, 1-up 2-right
+            attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x - 2, piecePos._z + 1)));
+            attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x + 2, piecePos._z + 1)));
+
+            // 1-down 2-left, 1-down 2-right
+            attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x - 2, piecePos._z - 1)));
+            attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x + 2, piecePos._z - 1)));
+
+            // 2-down 1-left, 2-down 1-right
+            attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x - 1, piecePos._z - 2)));
+            attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x + 1, piecePos._z - 2)));
+
+            return attackTiles;
+        }
+    }
 }
