c2ce47be01874b7e2c9669ae0b405d1abbc19345
Bora Sevim <sevimbora@gmail.com>
2024-07-17 11:08:57 +0300

    queen attack pattern implemented

--- Assets/_Scripts/Chess Pieces/Queen.cs
+++ Assets/_Scripts/Chess Pieces/Queen.cs
@@ -1,11 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using _Scripts.Grid_System;
 using UnityEngine;
 
 namespace ChessPieces
 {
     public class Queen : ChessPiece
     {
-        
+        public override List<GridObject> GetAttackPattern()
+        {
+            List<GridObject> attackTiles = new List<GridObject>();
+
+            var piecePos = this.GetGridPosition();
+            var _gridObject = m_gridSystem.GetGridObject(piecePos);
+
+            // Up
+            for(int zLimit = piecePos._z + 1; zLimit < 8; zLimit++)
+            {
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x, zLimit)));
+            }
+
+            // Down
+            for(int zLimit = piecePos._z + -1; zLimit >= 0; zLimit--)
+            {
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x, zLimit)));
+            }
+
+            // Left
+            for(int xLimit = piecePos._x - 1; xLimit >= 0; xLimit--)
+            {
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(xLimit, piecePos._z)));
+            }
+
+            // Right
+            for(int xLimit = piecePos._x + 1; xLimit < 8; xLimit++)
+            {
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(xLimit, piecePos._z)));
+            }
+
+            // Up-left
+            for(int xLimit = piecePos._x - 1, zLimit = piecePos._z + 1; xLimit >= 0 && zLimit < 8; xLimit--, zLimit++)
+            {
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(xLimit, zLimit)));
+            }
+
+            // Up-right
+            for(int xLimit = piecePos._x + 1, zLimit = piecePos._z + 1; xLimit < 8 && zLimit < 8; xLimit++, zLimit++)
+            {
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(xLimit, zLimit)));
+            }
+
+            // Down-left
+            for(int xLimit = piecePos._x - 1, zLimit = piecePos._z - 1; xLimit >= 0 && zLimit >= 0; xLimit--, zLimit--)
+            {
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(xLimit, zLimit)));
+            }
+
+            // Down-right
+            for(int xLimit = piecePos._x + 1, zLimit = piecePos._z - 1; xLimit < 0 && zLimit >= 8; xLimit++, zLimit--)
+            {
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(xLimit, zLimit)));
+            }
+            
+            return attackTiles;
+        }
     }
 }
