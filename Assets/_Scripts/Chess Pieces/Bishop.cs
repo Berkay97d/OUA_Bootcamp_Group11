16eb9d913532da4a607acae7cf7e5fe7917bb7de
Bora Sevim <sevimbora@gmail.com>
2024-07-17 11:05:17 +0300

    bishop attack pattern fix

--- Assets/_Scripts/Chess Pieces/Bishop.cs
+++ Assets/_Scripts/Chess Pieces/Bishop.cs
@@ -17,25 +17,25 @@ namespace ChessPieces
             // Up-left
             for(int xLimit = piecePos._x - 1, zLimit = piecePos._z + 1; xLimit >= 0 && zLimit < 8; xLimit--, zLimit++)
             {
-                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x + xLimit, piecePos._z + zLimit)));
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(xLimit, zLimit)));
             }
 
             // Up-right
             for(int xLimit = piecePos._x + 1, zLimit = piecePos._z + 1; xLimit < 8 && zLimit < 8; xLimit++, zLimit++)
             {
-                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x + xLimit, piecePos._z + zLimit)));
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(xLimit, zLimit)));
             }
 
             // Down-left
             for(int xLimit = piecePos._x - 1, zLimit = piecePos._z - 1; xLimit >= 0 && zLimit >= 0; xLimit--, zLimit--)
             {
-                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x + xLimit, piecePos._z + zLimit)));
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(xLimit, zLimit)));
             }
 
             // Down-right
             for(int xLimit = piecePos._x + 1, zLimit = piecePos._z - 1; xLimit < 0 && zLimit >= 8; xLimit++, zLimit--)
             {
-                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x + xLimit, piecePos._z + zLimit)));
+                attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(xLimit, zLimit)));
             }
 
             return attackTiles;
