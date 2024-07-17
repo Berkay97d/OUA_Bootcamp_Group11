5bed26fe2d2e89060e9d51f6475148b1f6900065
Bora Sevim <sevimbora@gmail.com>
2024-07-17 11:12:18 +0300

    pawn attack pattern implemented

--- Assets/_Scripts/Chess Pieces/Pawn.cs
+++ Assets/_Scripts/Chess Pieces/Pawn.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using _Scripts.Grid_System;
 using UnityEngine;
 
 namespace ChessPieces
 {
     public class Pawn : ChessPiece
     {
-        
+        public override List<GridObject> GetAttackPattern()
+        {
+            List<GridObject> attackTiles = new List<GridObject>();
+
+            var piecePos = this.GetGridPosition();
+            var _gridObject = m_gridSystem.GetGridObject(piecePos);
+
+            attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x + 1, piecePos._z + 1)));
+            attackTiles.Add(m_gridSystem.GetGridObject(new GridPosition(piecePos._x - 1, piecePos._z + 1)));
+            
+            return attackTiles;
+        }
     }
 }
