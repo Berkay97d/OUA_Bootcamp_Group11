8c226f8399acb1c2d296238d2691b5d913bc985b
Bora Sevim <sevimbora@gmail.com>
2024-07-30 13:38:16 +0300

    added flags for fire and special

--- Assets/_Scripts/Chess Pieces/ChessPiece.cs
+++ Assets/_Scripts/Chess Pieces/ChessPiece.cs
@@ -10,14 +10,35 @@ namespace ChessPieces
     public abstract class ChessPiece : Unit
     {
         protected GridSystem m_gridSystem;
+        protected bool[] pieceStatus = new bool[] { false, false };
+
         private void Start()
         {
             m_gridSystem = ChessGrid.GetGridSystem();
         }
+
+        public void SetPieceStatus(bool status, int index)
+        {
+            pieceStatus[index] = status;
+        }
+        public bool[] GetPieceStatus()
+        {
+            return pieceStatus;
+        }
+
         public virtual List<GridObject> GetAttackPattern()
         {
             return null;
         }
-        
+
+        public virtual List<GridObject> GetBishopPattern()
+        {
+            return null;
+        }
+
+        public virtual List<GridObject> GetKnightPattern()
+        {
+            return null;
+        }
     }
 }
