540616bb6b2e77ae1dbdf24a287cfda61abd1e37
Bora Sevim <sevimbora@gmail.com>
2024-07-30 13:40:39 +0300

    implemented flags and special king movement

--- Assets/_Scripts/Chess Pieces/ChessPieceMovement.cs
+++ Assets/_Scripts/Chess Pieces/ChessPieceMovement.cs
@@ -15,32 +15,64 @@ namespace ChessPieces
 
         public static event Action<ChessPiece, GridObject, GridObject> OnChessPieceMove; 
 
-        public static event Action OnKingWin; 
+        public static event Action<ChessPiece, GridObject, List<GridObject>> OnSpecialKingMove;
+
+        public static event Action OnKingWin;
+
+        private List<GridObject> movableGrids;
+        private GridObject currentGridObject;
 
         private void Start()
         {
             _chessPiece = GetComponent<ChessPiece>();
             m_gridSystem = ChessGrid.GetGridSystem();
             GameInput.m_instance.OnMoveInput += Movement;
+
+            currentGridObject = m_gridSystem.GetGridObject(_chessPiece.GetGridPosition());
+        }
+
+        // CREATED FOR TEST PURPOSES
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.O) && _chessPiece.GetTurn() && !_chessPiece.GetPieceStatus()[0])
+            {
+                if(_chessPiece is King && _chessPiece.team == Team.White && !_chessPiece.GetPieceStatus()[1])
+                {
+                    _chessPiece.SetPieceStatus(true, 1);
+                    _chessPiece.SetPieceStatus(false, 0);
+                    movableGrids = _chessPiece.GetBishopPattern();
+                    OnSpecialKingMove?.Invoke(_chessPiece, currentGridObject, movableGrids);
+                }
+            }
+
+            else if(Input.GetKeyDown(KeyCode.P) && _chessPiece.GetTurn() && !_chessPiece.GetPieceStatus()[0])
+            {
+                if(_chessPiece is King && _chessPiece.team == Team.White && !_chessPiece.GetPieceStatus()[1])
+                {
+                    _chessPiece.SetPieceStatus(true, 1);
+                    _chessPiece.SetPieceStatus(false, 0);
+                    movableGrids = _chessPiece.GetKnightPattern();
+                    OnSpecialKingMove?.Invoke(_chessPiece, currentGridObject, movableGrids);
+                }
+            }
         }
 
         private void Movement()
         {
-            if(_chessPiece.GetTurn())
+            if(_chessPiece.GetTurn() && !_chessPiece.GetPieceStatus()[0])
             {
                 GridObject selectedGridObject = GridObjectSelectionSystem.GetSelectedGridObject();
-                MoveTo(selectedGridObject);
+                if(!_chessPiece.GetPieceStatus()[1])
+                    movableGrids = currentGridObject.GetMovableGrids();
+                MoveTo(selectedGridObject, movableGrids);
             }
         }
         
-        public void MoveTo(GridObject gridObject)
-        {
-            var currentGridObject = m_gridSystem.GetGridObject(_chessPiece.GetGridPosition());
-            var _movableGrids = currentGridObject.GetMovableGrids();
-            
+        public void MoveTo(GridObject gridObject, List<GridObject> _movableGrids)
+        {   
             if(_movableGrids.Contains(gridObject))
             {
-                GridObject prevGridObject = m_gridSystem.GetGridObject(_chessPiece.GetGridPosition());
+                GridObject prevGridObject = currentGridObject;
                 prevGridObject.SetIsOccupied(false);
 
                 if(prevGridObject.GetVisitCount() >= 2)
@@ -49,8 +81,12 @@ namespace ChessPieces
                 Vector3 movedPosition = m_gridSystem.GetWorldPositionFromGridPosition(gridObject.GetGridPosition());
                 var movedGridPosition = m_gridSystem.GetGridPositionFromWorldPosition(movedPosition);
                 var movedGridObject = m_gridSystem.GetGridObject(movedGridPosition);
+
+                currentGridObject = movedGridObject;
                 _chessPiece.SetPosition(movedPosition);
                 gridObject.SetIsOccupied(true);
+
+                _chessPiece.SetPieceStatus(false, 1);
 
                 if (_chessPiece is King && movedGridPosition._z == 7)
                 {
@@ -63,6 +99,5 @@ namespace ChessPieces
             else
                 Debug.Log("MOVE FAILED");
         }
-
     }
 }
